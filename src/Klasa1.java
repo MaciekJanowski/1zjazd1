47571ef99b90d6aeacdaed172584984457d66520
Maciej Janowski <maciekjanowski3@gmail.com>
2024-10-13 09:54:42 +0200

    3commit

--- src/Klasa1.java
+++ src/Klasa1.java
@@ -1,5 +1,5 @@
 public class Klasa1 {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula i IT");
+        System.out.println("Hello Vistula i IT i tak");
     }
 }
